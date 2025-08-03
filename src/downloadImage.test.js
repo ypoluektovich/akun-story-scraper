4c86d39021593721afa88079e5aaabc0bbc8d914
Yanus Poluektovich <ypoluektovich@gmail.com>
2025-08-03 14:20:32 +0300

    review fixes
    To get it running on Windows:
    - fixed the quotation on test script in package.json
    - fixed the path check

--- src/downloadImage.test.js
+++ src/downloadImage.test.js
@@ -1,6 +1,7 @@
 import { describe, it, before, beforeEach, mock } from 'node:test';
 import assert from 'node:assert/strict';
 import path from "path";
+import { platform } from "process";
 import { createHash } from 'node:crypto';
 
 // this test uses an in-memory file system to avoid polluting CWD.
@@ -13,6 +14,12 @@ describe('downloadImage', () => {
 
     function dl(url) {
         return downloadImage(url, 'tmp');
+    }
+    function assertPath(p, expectedPath) {
+        if (platform === 'win32') {
+            expectedPath = expectedPath.replaceAll("/", "\\");
+        }
+        assert.equal(p, expectedPath);
     }
     async function assertSize(p, expectedSize) {
         const stat = await fs.stat(path.join('tmp', p));
@@ -59,14 +66,14 @@ describe('downloadImage', () => {
 
     it('should download an image and return its path', async () => {
         const p = await dl('https://cdn6.fiction.live/file/fictionlive/6dddb2ff-bdf2-418f-9a6d-710ca1c35acb.jpg');
-        assert.equal(p, 'cdn6.fiction.live/file/fictionlive/6dddb2ff-bdf2-418f-9a6d-710ca1c35acb.jpg');
+        assertPath(p, 'cdn6.fiction.live/file/fictionlive/6dddb2ff-bdf2-418f-9a6d-710ca1c35acb.jpg');
         await assertSize(p, 116972);
         await assertHash(p, 'b282d2692dfda5d0e0e18370a7c42e2f861dbfc28e5fa83a323d55aa4145eb42');
     });
 
     it('should detect file type and append the extension', async () => {
         const p = await dl('https://cdn6.fiction.live/file/fictionlive/447506bd-f804-4312-9f9e-726dc7daa745.jpeg');
-        assert.equal(p, 'cdn6.fiction.live/file/fictionlive/447506bd-f804-4312-9f9e-726dc7daa745.jpeg.jpg');
+        assertPath(p, 'cdn6.fiction.live/file/fictionlive/447506bd-f804-4312-9f9e-726dc7daa745.jpeg.jpg');
         await assertSize(p, 317425);
         await assertHash(p, '8340b70cf3e54001ee7a750ed7a6a86b590ff7f11cb31a63d31811dd2618c821');
     });
